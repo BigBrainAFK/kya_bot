a9801735230dbb0d33a4e7663a80810686b4869a
Tony Langhammer <tony.langhammer@yahoo.de>
2022-11-05 12:09:50 +0100

    Fix command deletion script

--- src/scripts/delete_all_commands.js
+++ src/scripts/delete_all_commands.js
@@ -1,18 +1,20 @@
-require('dotenv').config()
-const { REST } = require('@discordjs/rest');
-const { Routes } = require('discord-api-types/v9');
+require("dotenv").config();
+const { REST } = require("@discordjs/rest");
+const { Routes } = require("discord-api-types/v9");
 
 const token = process.env.TOKEN;
-const clientId = '';
-const guildId = '';
-    
-const rest = new REST({ version: '9' }).setToken(token);
+const clientId = "";
+const guildId = "";
 
-rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: [] })
-	.then(() => console.log('Successfully deleted all guild commands.'))
-	.catch(console.error);
+const rest = new REST({ version: "10" }).setToken(token);
+
+rest
+  .put(Routes.applicationGuildCommands(clientId, guildId), { body: [] })
+  .then(() => console.log("Successfully deleted all guild commands."))
+  .catch(console.error);
 
 // for global commands
-rest.put(Routes.applicationCommands(clientId), { body: [] })
-	.then(() => console.log('Successfully deleted all application commands.'))
-	.catch(console.error);
+rest
+  .put(Routes.applicationCommands(clientId), { body: [] })
+  .then(() => console.log("Successfully deleted all application commands."))
+  .catch(console.error);
